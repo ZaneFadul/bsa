204e770fc71287421072fbd1e5b7c47234aefaea
ZaneFadul <theboonandthezane@gmail.com>
2020-09-19 18:16:24 -0400

    Added files for pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Colors from './constants/Colors';
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App"
       style={{
         background: 'linear-gradient(white,transparent)',
         backgroundColor: `${Colors.pink}`,
         }}>
 
-    </div>
+      </div>
+      </Router>
   );
 }
 
