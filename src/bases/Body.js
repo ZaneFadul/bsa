115f8785da35bdf0c1f706eb89995dbada61e419
ZaneFadul <theboonandthezane@gmail.com>
2020-09-20 00:26:41 -0400

    Working on nav bar

--- src/bases/Body.js
+++ src/bases/Body.js
@@ -1,3 +1,14 @@
 import React, { Component } from 'react';
 import Colors from '../constants/Colors';
 
+class Body extends Component {
+  render() {
+    return (
+      <div className="Body">
+        {this.props.content}
+      </div>
+    );
+  }
+}
+
+export default Body;
